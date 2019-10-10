4f5936a45c48fcefe87d3eb7753a0222a2205156
Laura Colten <lauracolten5@gmail.com>
2019-10-10 14:49:04 -0500

    first commit

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -4,6 +4,12 @@ const user = (state = null) => state
 
 // add switch statements in here
 const cars = (state = [], action) => {
+    switch(action.type) {
+        case 'ADD_CAR':
+            return [ ...state, action.value]
+        default:
+            return state
+    }
     return state
 }
 
